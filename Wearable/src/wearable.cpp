7c5a1f7aeaa7c6c22828f32d61bfc302fd598ba1
MrDoritos <ianmoore322@gmail.com>
2025-07-09 20:38:52 -0700

    Successful flash

--- Wearable/src/wearable.cpp
+++ Wearable/src/wearable.cpp
@@ -1,14 +1,22 @@
 #include <stdio.h>
+#include <inttypes.h>
 #include "esp_flash.h"
 #include "esp_system.h"
 #include "esp_chip_info.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
+extern "C" {
 void app_main() {
     printf("test\n");
 
     uint32_t flash_size;
     esp_flash_get_size(0, &flash_size);
-    printf("%iMB flash size\n", flash_size / (1024 * 1024));
+    printf("%luMB flash size\n", flash_size / (1024 * 1024));
     
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+
     fflush(stdout);
+    esp_restart();
+}
 }
