052b9d543c6769fc06b6ce79ddc2f6f4862c2cda
MrDoritos <ianmoore322@gmail.com>
2025-07-31 01:38:42 -0700

    Set accelerometer offsets

--- Wearable/src/peripheral/gps_imu.cpp
+++ Wearable/src/peripheral/gps_imu.cpp
@@ -25,19 +25,32 @@ esp_err_t GPSIMU::init() {
     if (s_lsm6dsl.Enable_X()) return 1;
     if (s_lsm6dsl.Enable_G()) return 1;
 
+    if (s_lsm6dsl.Set_X_ODR(12.5) || s_lsm6dsl.Set_G_ODR(12.5) || s_lsm6dsl.Set_X_FS(4.0))
+        return 1;
+
+    s_lsm6dsl.WriteReg(0x73, 39);
+    s_lsm6dsl.WriteReg(0x74, 49);
+    s_lsm6dsl.WriteReg(0x75, 38);
+    /*
+    s_lsm6dsl.WriteReg(0x2D, 2017&0xff);
+    s_lsm6dsl.WriteReg(0x2E, 2017>>8);
+    s_lsm6dsl.WriteReg(0x2F, 455&0xff);
+    s_lsm6dsl.WriteReg(0x30, 455>>8);
+    */
+
     return ESP_OK;
 }
 
 AxisData GPSIMU::getGyroscope() {
     int32_t axis[3];
     s_lsm6dsl.Get_G_Axes(axis);
-    return AxisData(axis[0], axis[1], axis[2]);
+    return AxisData(axis[0] * 0.001, axis[1] * 0.001, axis[2] * 0.001);
 }
 
 AxisData GPSIMU::getAccelerometer() {
     int32_t axis[3];
     s_lsm6dsl.Get_X_Axes(axis);
-    return AxisData(axis[0], axis[1], axis[2]);
+    return AxisData(axis[0] * 0.001, axis[1] * 0.001, axis[2] * 0.001);
 }
 
 }
